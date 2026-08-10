ab2d55fbff04eccc635609fd6a6a255d1284c846
German <jg345762@gmail.com>
2026-08-10 14:53:48 -0300

    feat: configure Firebase for production

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,11 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString(
+    "utf8",
+  ),
+);
 
 const firebaseApp = initializeApp({
   credential: cert(serviceAccount),
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
+import "dotenv/config";
 import app from "./app.js";
-import dotenv from "dotenv";
 import db from "./config/firebase.js";
-
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
